bf13ae6c47022ac4baa9fe718215bbde61a8949a
Shashank <shashank.daima@redacto.io>
2025-12-24 19:13:19 +0530

    updated keys according req

--- src/redacto/events/events.py
+++ src/redacto/events/events.py
@@ -5,10 +5,10 @@ class EventType(str, Enum):
     TEST_EVENT = "test.event"
     DOCUMENT_EXPIRED = "platform.document.expired"
     VRM_FORM_SUBMITTED = "vrm.form.submitted"
-    VRM_VENDOR_TIER_CHANGED = "vrm.vendor__tier.changed"
-    VRM_VENDOR_SCORE_CHANGED = "vrm.vendor__score.changed"
-    VRM_ASSESSMENT_STATUS_CHANGED = "vrm.assessment__status.changed"
-    VRM_COMMENT_UPSERTED = "vrm.comments.upserted"
+    VRM_VENDOR_TIER_UPDATED = "vrm.vendor.tier.updated"
+    VRM_VENDOR_SCORE_UPDATED = "vrm.vendor.score.updated"
+    VRM_ASSESSMENT_STATUS_UPDATED = "vrm.assessment.status.updated"
+    VRM_COMMENT_UPSERTED = "vrm.form.comments.upserted"
 
     def __str__(self):
         return self.value
