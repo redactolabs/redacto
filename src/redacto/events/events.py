b087250bd5e6ce23b5c1d5d83f22b402462cb805
Shashank <shashank.daima@redacto.io>
2025-12-24 18:36:31 +0530

    chore(redacto): make reformat

--- src/redacto/events/events.py
+++ src/redacto/events/events.py
@@ -5,10 +5,10 @@ class EventType(str, Enum):
     TEST_EVENT = "test.event"
     DOCUMENT_EXPIRED = "platform.document.expired"
     VRM_FORM_SUBMITTED = "vrm.form.submitted"
-    VRM_VENDOR_TIER_CHANGED="vrm.vendor__tier.changed"
-    VRM_VENDOR_SCORE_CHANGED="vrm.vendor__score.changed"
-    VRM_ASSESSMENT_STATUS_CHANGED="vrm.assessment__status.changed"
-    VRM_COMMENT_UPSERTED="vrm.comments.upserted"
+    VRM_VENDOR_TIER_CHANGED = "vrm.vendor__tier.changed"
+    VRM_VENDOR_SCORE_CHANGED = "vrm.vendor__score.changed"
+    VRM_ASSESSMENT_STATUS_CHANGED = "vrm.assessment__status.changed"
+    VRM_COMMENT_UPSERTED = "vrm.comments.upserted"
 
     def __str__(self):
         return self.value
