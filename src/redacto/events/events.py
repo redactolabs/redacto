f6c073798c7606aea5a6940993ff03dd1dd73bb0
Shashank <shashank.daima@redacto.io>
2025-12-26 03:21:29 +0530

    minor

--- src/redacto/events/events.py
+++ src/redacto/events/events.py
@@ -9,7 +9,9 @@ class EventType(str, Enum):
     VRM_VENDOR_SCORE_UPDATED = "vrm.vendor.score.updated"
     VRM_ASSESSMENT_STATUS_UPDATED = "vrm.assessment.status.updated"
     VRM_COMMENT_UPSERTED = "vrm.form.comments.upserted"
+
     VRM_EXECUTE_POLICY = "vrm.policy.execute"
+    VRM_EVALUATE_RULE = "vrm.rule.evaluate"
 
     def __str__(self):
         return self.value
